53db89a51c97b419681761141fbb0881601d83a0
ropert911 <ropert911@163.com>
2019-03-04 21:00:53 +0800

    添加示例

--- JDK/src/main/java/com/xq/study/jdk/容器/数组Array.java
+++ JDK/src/main/java/com/xq/study/jdk/容器/数组Array.java
@@ -1,6 +1,10 @@
 package com.xq.study.jdk.容器;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by xq on 2019/2/27.
@@ -8,7 +12,29 @@ import java.lang.reflect.Array;
 public class 数组Array {
     public static void main(String[] args) {
         useArray();
+
+        testArray2Set(); //数组转Set
+        testArray2List();   //数组转list
     }
+
+    private static void testArray2Set() {
+        String[] arr = {"AA", "BB", "DD", "CC", "BB"};
+
+        //数组-->Set
+        Set<String> set = new HashSet<String>(Arrays.asList(arr));
+        System.out.println(set);
+    }
+
+    private static void testArray2List() {
+        //数组-->List
+        String[] ss = {"JJ", "KK"};
+        List<String> list1 = Arrays.asList(ss);
+        List<String> list2 = Arrays.asList("AAA", "BBB");
+        System.out.println(list1);
+        System.out.println(list2);
+    }
+
+
 
     public static void useArray() {
         int[][][] matrix1 = (int[][][]) Array.newInstance(int.class, 3, 3, 3);
