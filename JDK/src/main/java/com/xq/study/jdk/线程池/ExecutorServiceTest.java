8b674bd6a64cc6ef1de052d28d3ea3a066a4383a
ropert911 <ropert911@163.com>
2019-03-05 16:30:18 +0800

    添加示例

--- JDK/src/main/java/com/xq/study/jdk/线程池/ExecutorServiceTest.java
+++ JDK/src/main/java/com/xq/study/jdk/线程池/ExecutorServiceTest.java
@@ -14,6 +14,8 @@ import java.util.concurrent.*;
  */
 public class ExecutorServiceTest {
     public static void main(String[] args) {
+//        ExecutorService es = Executors.newFixedThreadPool(10);    //指标线程数量
+
         useThreadRunable(); //一个一个加任务:添加 Runaal任务
         useThread1();   //一个一个加任务:添加 Thread任务
         useThread2();  //批量添加要执行的任务
