8b100f4c40c8def2f89d428082f65fd56b072be4
ropert911 <ropert911@163.com>
2019-09-04 09:47:11 +0800

    Log示例调整

--- Demo-Unitest/Demo2-TestNG/src/test/java/com/study/unittest/apitest2/emoapitest2/controller/ISMUserController4Test.java
+++ Demo-Unitest/Demo2-TestNG/src/test/java/com/study/unittest/apitest2/emoapitest2/controller/ISMUserController4Test.java
@@ -3,8 +3,11 @@ package com.study.unittest.apitest2.emoapitest2.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -17,20 +20,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest //(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@SpringBootTest//(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ISMUserController4Test extends AbstractTestNGSpringContextTests{
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
-    private WebApplicationContext wac;
     private MockMvc mockMvc;
-
-    @BeforeTest
-    public void BeforeTest() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-
-    }
-
 
     @Test
     public void testBook1() throws Exception {
