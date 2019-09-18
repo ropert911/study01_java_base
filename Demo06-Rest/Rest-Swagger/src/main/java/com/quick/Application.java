6cc7d20d75be57198b4e955f2a1d2a1245a3fa95
ropert911 <ropert911@163.com>
2019-09-18 19:19:24 +0800

    添加根据swagger文档生成 pdf,html相关的操作说明

--- Demo06-Rest/Rest-Swagger/src/main/java/com/quick/Application.java
+++ Demo06-Rest/Rest-Swagger/src/main/java/com/quick/Application.java
@@ -11,29 +11,12 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * @author xiaoqian
+ * @date 2019/0918
+ */
 @SpringBootApplication
-@EnableSwagger2
 public class Application {
-
-    @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.quick.api")) //扫描API的包路径
-                .paths(PathSelectors.any())
-                .build();
-    }
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("SpringBoot整合Swagger2") // 标题
-                .description("api接口的文档整理，支持在线测试") // 描述
-                .termsOfServiceUrl("http://vector4wang.tk/") //网址
-                .contact("Vector.Wang") // 作者
-                .version("1.0") // 版本号
-                .build();
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
