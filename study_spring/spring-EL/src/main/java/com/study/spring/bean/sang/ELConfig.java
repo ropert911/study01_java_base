2ed2e2a47d96608d8edeb8685034b2b5ef500c12
ropert911 <ropert911@163.com>
2019-09-30 13:42:19 +0800

    代码调整

--- study_spring/spring-EL/src/main/java/com/study/spring/bean/sang/ELConfig.java
+++ study_spring/spring-EL/src/main/java/com/study/spring/bean/sang/ELConfig.java
@@ -12,52 +12,68 @@ import org.springframework.core.io.Resource;
 import java.io.IOException;
 
 /**
- * Created by sang on 16-12-13.
+ * EL 表达式  使用t.properties文件
+ *
+ * @author sk-qianxiao
  */
 @Configuration
 @ComponentScan("com.study.spring.bean.sang")
-@PropertySource(value = "t.properties",encoding = "UTF-8")
+@PropertySource(value = "t.properties", encoding = "UTF-8")
 public class ELConfig {
-    @Value("I Love You!")
-    private String normal;
+    /**
+     * 使用系统属性
+     */
     @Value("#{systemProperties['os.name']}")
     private String osName;
+    /**
+     * 使用环境变量
+     */
     @Value("#{systemEnvironment['os.arch']}")
     private String osArch;
+    /**
+     * 调用参数
+     */
     @Value("#{T(java.lang.Math).random()*100}")
     private double randomNumber;
-    @Value("#{demoService1.author}")
-    private String author;
-    @Value("t.txt")
-    private Resource testFile;
 
-    @Value("http://www.baidu.com")
-    private Resource testUrl;
+
+    /**
+     * 使用属性文件中的值
+     */
     @Value("${sang.username}")
     private String su;
     @Value("${sang.password}")
     private String sp;
-    @Value("${sang.nickname}")
+    @Value("张二娃")
     private String sn;
     @Autowired
     private Environment environment;
 
+
+    /**
+     * 使用t.txt文件资源
+     */
+    @Value("t.txt")
+    private Resource testFile;
+    /**
+     * url当资源
+     */
+    @Value("http://www.baidu.com")
+    private Resource testUrl;
+
     public void output() {
         try {
-            System.out.println(normal);
             System.out.println(osName);
             System.out.println(osArch);
             System.out.println(randomNumber);
-            System.out.println(author);
-            System.out.println(IOUtils.toString(testFile.getInputStream(),"UTF-8"));
-            //访问网址
-            System.out.println(IOUtils.toString(testUrl.getInputStream(),"UTF-8"));
-            //获取网址
-            System.out.println("testUrl.getURL():"+testUrl.getURL());
             System.out.println(su);
             System.out.println(sp);
             System.out.println(sn);
-            System.out.println(environment.getProperty("sang.nickname"));
+            System.out.println(environment.getProperty("sang.password"));
+
+            System.out.println(IOUtils.toString(testFile.getInputStream(), "UTF-8"));
+            System.out.println(IOUtils.toString(testUrl.getInputStream(), "UTF-8"));
+            System.out.println("testUrl.getURL():" + testUrl.getURL());
         } catch (IOException e) {
             e.printStackTrace();
         }
