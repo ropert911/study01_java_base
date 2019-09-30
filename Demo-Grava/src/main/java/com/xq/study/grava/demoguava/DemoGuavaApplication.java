63efbe173994874e1ebeda2b67f382d2086ea3e2
ropert911 <ropert911@163.com>
2019-09-30 15:24:50 +0800

    代码调整

---
+++ Demo-Grava/src/main/java/com/xq/study/grava/demoguava/DemoGuavaApplication.java
@@ -0,0 +1,16 @@
+package com.xq.study.grava.demoguava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sk-qianxiao
+ */
+@SpringBootApplication
+public class DemoGuavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGuavaApplication.class, args);
+    }
+
+}
