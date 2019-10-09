b06e9ca73d32bace01c80c6725c527790b2d931f
ropert911 <ropert911@163.com>
2019-10-09 18:33:29 +0800

    代码调整

---
+++ Demo01-JDK-Base/src/test/java/com/xq/study/jdk/UUIDTest.java
@@ -0,0 +1,13 @@
+package com.xq.study.jdk;
+
+import java.util.UUID;
+
+/**
+ * @author sk-qianxiao
+ */
+public class UUIDTest {
+    public static void main(String[] args) {
+        String batchUUID = UUID.randomUUID().toString();
+        System.out.println(batchUUID);
+    }
+}
