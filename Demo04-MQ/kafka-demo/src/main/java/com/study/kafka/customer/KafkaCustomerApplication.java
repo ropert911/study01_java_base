29b585174fb56bbe07cccdb6a6de5d07579dd034
root <254323626@qq.com>
2019-10-13 09:22:18 +0800

    代码调整

--- Demo04-MQ/kafka-demo/src/main/java/com/study/kafka/customer/KafkaCustomerApplication.java
+++ Demo04-MQ/kafka-demo/src/main/java/com/study/kafka/customer/KafkaCustomerApplication.java
@@ -2,8 +2,13 @@ package com.study.kafka.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * @author xq
+ */
 @SpringBootApplication
+@EnableScheduling
 public class KafkaCustomerApplication {
 
 	public static void main(String[] args) {
