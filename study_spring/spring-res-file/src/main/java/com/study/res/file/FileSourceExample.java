f8af73f53cef8db9d22b0dc6972397daa271c193
ropert911 <ropert911@163.com>
2019-10-16 19:47:57 +0800

    代码调整

--- study_spring/spring-res-file/src/main/java/com/study/res/file/FileSourceExample.java
+++ study_spring/spring-res-file/src/main/java/com/study/res/file/FileSourceExample.java
@@ -7,10 +7,9 @@ import org.springframework.core.io.WritableResource;
 import org.springframework.core.io.support.EncodedResource;
 import org.springframework.util.FileCopyUtils;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.util.List;
 
 /**
  * @author sk-qianxiao
@@ -38,20 +37,44 @@ public class FileSourceExample {
 
                 System.out.println("res1 name=>" + res1.getFilename());
             }
-            // 以类路径的方式访问
+
+            Resource resource = new ClassPathResource("conf/file1");
+            System.out.println("文件名:" + resource.getFilename());
+            System.out.println("描述:" + resource.getDescription());
+            System.out.println("正文长度:" + resource.contentLength());
+
+
             {
-                Resource res2 = new ClassPathResource("conf/file1");
-                InputStream ins2 = res2.getInputStream();
-                System.out.println("res2 name=>" + res2.getFilename());
+                System.out.println("以classpath方式访问----使用inputstream读取=======");
+                InputStream inputStream = resource.getInputStream();
+                int contentLen = inputStream.available();
+                byte[] st = new byte[contentLen];
+                inputStream.read(st);
+                System.out.println(new String(st));
             }
-            // 以类路径的方式访问
             {
-                Resource res = new ClassPathResource("conf/file1");
-                EncodedResource encRes = new EncodedResource(res, "UTF-8");
+                System.out.println("以类路径的方式访问---使用FileCopyUtils读取=======");
+                EncodedResource encRes = new EncodedResource(resource, "UTF-8");
                 String content = FileCopyUtils.copyToString(encRes.getReader());
                 System.out.println(content);
             }
-
+            {
+                System.out.println("使用Files.readAllLines方式读取=======");
+                File file = resource.getFile();
+                List<String> list = Files.readAllLines(file.toPath());
+                for (String string : list) {
+                    System.out.println(string);
+                }
+            }
+            {
+                System.out.println("使用FileReader进行读取=======");
+                File file = resource.getFile();
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String str = "";
+                while ((str = br.readLine()) != null) {
+                    System.out.println(str);
+                }
+            }
         } catch (
                 IOException e) {
             e.printStackTrace();
