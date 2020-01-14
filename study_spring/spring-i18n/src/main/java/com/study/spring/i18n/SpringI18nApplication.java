2522dfd5d8658f1eda5c3beea637f70b9ffc9e4b
ropert911 <ropert911@163.com>
2020-01-14 11:04:18 +0800

    代码结构调整

---
+++ study_spring/spring-i18n/src/main/java/com/study/spring/i18n/SpringI18nApplication.java
@@ -0,0 +1,15 @@
+package com.study.spring.i18n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sk-qianxiao
+ */
+@SpringBootApplication
+public class SpringI18nApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringI18nApplication.class, args);
+	}
+}
