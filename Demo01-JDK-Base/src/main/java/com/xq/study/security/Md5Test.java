7eef96b6bb8436f1ed04a23eab8356fcbccb9f26
ropert911 <ropert911@163.com>
2020-01-15 13:42:32 +0800

    添加MD5的示例

---
+++ Demo01-JDK-Base/src/main/java/com/xq/study/security/Md5Test.java
@@ -0,0 +1,42 @@
+package com.xq.study.security;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author sk-qianxiao
+ * @date 2020/1/15
+ */
+public class Md5Test {
+    public static void main(String[] args) {
+        String input = "123456";
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.reset();
+            messageDigest.update(input.getBytes("UTF-8"));
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("NoSuchAlgorithmException caught!");
+            System.exit(-1);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        //进行MD5转换
+        byte[] byteArray = messageDigest.digest();
+
+        StringBuffer md5StrBuff = new StringBuffer();
+        for (int i = 0; i < byteArray.length; i++) {
+            if (Integer.toHexString(0xFF & byteArray[i]).length() == 1) {
+                md5StrBuff.append("0").append(Integer.toHexString(0xFF & byteArray[i]));
+            } else {
+                md5StrBuff.append(Integer.toHexString(0xFF & byteArray[i]));
+            }
+
+        }
+        String output = md5StrBuff.toString().toLowerCase();
+
+        System.out.println(output);
+    }
+}
