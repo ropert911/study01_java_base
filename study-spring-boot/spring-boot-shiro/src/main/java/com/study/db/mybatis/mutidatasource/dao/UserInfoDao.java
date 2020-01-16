a5ee22336a30aab87e340314f28ff0f799075205
ropert911 <ropert911@163.com>
2020-01-16 10:08:14 +0800

    代码调整

--- study-spring-boot/spring-boot-shiro/src/main/java/com/study/db/mybatis/mutidatasource/dao/UserInfoDao.java
+++
@@ -1,9 +0,0 @@
-package com.study.db.mybatis.mutidatasource.dao;
-
-import com.study.db.mybatis.mutidatasource.entity.UserInfo;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserInfoDao extends CrudRepository<UserInfo,Long> {
-    /**通过username查找用户信息;*/
-    public UserInfo findByUsername(String username);
-}
