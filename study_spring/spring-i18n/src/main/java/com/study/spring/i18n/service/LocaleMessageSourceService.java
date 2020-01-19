c5d0c6fb9240bed0c146d00c96d58ac62265f7b2
ropert911 <ropert911@163.com>
2020-01-19 16:46:43 +0800

    代码优化

--- study_spring/spring-i18n/src/main/java/com/study/spring/i18n/service/LocaleMessageSourceService.java
+++ study_spring/spring-i18n/src/main/java/com/study/spring/i18n/service/LocaleMessageSourceService.java
@@ -9,17 +9,12 @@ import java.util.Locale;
 
 /**
  * 国际化工具类
- *
- * @author Angel--守护天使
- * @version v.0.1
- * @date 2016年8月5日下午2:44:03
  */
 @Component
 public class LocaleMessageSourceService {
 
     @Resource
     private MessageSource messageSource;
-
 
 
     public String getMessage(String code, String defaultMessage) {
@@ -34,18 +29,11 @@ public class LocaleMessageSourceService {
         return getMessage(code, null, "", locale);
     }
 
-    /**
-     * @param code：对应messages配置的key.
-     * @return
-     */
+
     public String getMessage(String code) {
         return getMessage(code, new Object[]{});
     }
-    /**
-     * @param code：对应messages配置的key.
-     * @param args                   :数组参数.
-     * @return
-     */
+
     public String getMessage(String code, Object[] args) {
         return getMessage(code, args, "");
     }
@@ -55,27 +43,12 @@ public class LocaleMessageSourceService {
     }
 
 
-    /**
-     * @param code：对应messages配置的key.
-     * @param args                   :数组参数.
-     * @param defaultMessage         :没有设置key的时候的默认值.
-     * @return
-     */
     public String getMessage(String code, Object[] args, String defaultMessage) {
-        //这里使用比较方便的方法，不依赖request.
         Locale locale = LocaleContextHolder.getLocale();
         return getMessage(code, args, defaultMessage, locale);
     }
 
-    /**
-     * 指定语言.
-     *
-     * @param code
-     * @param args
-     * @param defaultMessage
-     * @param locale
-     * @return
-     */
+
     public String getMessage(String code, Object[] args, String defaultMessage, Locale locale) {
         return messageSource.getMessage(code, args, defaultMessage, locale);
     }
