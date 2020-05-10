92a802d72cc0ed054e0494596608bec0b12a1f72
ropert911 <ropert911@163.com>
2020-05-10 16:20:24 +0800

    添加File使用示例

--- Demo01-JDK-Base/src/main/java/com/study/base/TestApplication.java
+++ Demo01-JDK-Base/src/main/java/com/study/base/TestApplication.java
@@ -3,15 +3,11 @@ package com.study.base;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.Console;
-
 public class TestApplication {
     private static Logger logger = LoggerFactory.getLogger(TestApplication.class);
 
-    public static void main(String[] args) {
-        char[] src = new char[]{'a', 'b', 'c', 'd', 'e'};
-        char[] des = new char[5];
-        System.arraycopy(src, 0, des, 0, 4);
-        System.out.println(des);
+    public static void main(String[] args) throws Exception {
+
+
     }
 }
