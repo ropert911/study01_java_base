02c6f923b4f16f822841c5980dd5a65bf06a099b
ropert911 <ropert911@163.com>
2021-09-06 14:29:29 +0800

    修改

--- Demo01-JDK-Base/src/main/java/com/study/base/patterns/SingletonEnum.java
+++
@@ -1,9 +0,0 @@
-package com.study.base.patterns;
-
-/**
- * @author sk-qianxiao
- * @date 2020/1/7
- */
-public enum SingletonEnum {
-    STANCE;
-}
--- Demo01-JDK-Base/src/main/java/com/study/base/patterns/factory/RoleOp.java
+++
@@ -1,9 +0,0 @@
-package com.study.base.patterns.factory;
-
-/**
- * @author sk-qianxiao
- * @date 2020/1/7
- */
-public interface RoleOp {
-    void handle();
-}
