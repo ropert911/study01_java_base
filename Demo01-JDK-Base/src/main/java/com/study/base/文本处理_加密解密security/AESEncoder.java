974fa00091b8af7bbc9661ff68b76043e0adabf3
ropert911 <ropert911@163.com>
2021-09-30 13:27:53 +0800

    修改

--- Demo01-JDK-Base/src/main/java/com/study/base/文本处理_加密解密security/AESEncoder.java
+++ Demo01-JDK-Base/src/main/java/com/study/base/文本处理_加密解密security/AESEncoder.java
@@ -3,6 +3,7 @@ package com.study.base.文本处理_加密解密security;
 import org.apache.commons.codec.binary.Base64;
 
 import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -66,13 +67,43 @@ public class AESEncoder {
         return null;
     }
 
-    /*
-     * 解密
-     * 解密过程：
-     * 1.同加密1-4步
-     * 2.将加密后的字符串反纺成byte[]数组
-     * 3.将加密内容解密
-     */
+    public static String AESEncode(String content, byte[] password, byte[] iv) {
+        try {
+            //5.根据字节数组生成AES密钥
+            SecretKeySpec key = new SecretKeySpec(password, "AES");
+            //使用CBC模式，需要一个向量iv，可增加加密算法的强度
+            IvParameterSpec ips = new IvParameterSpec(iv);
+            //6.根据指定算法AES自成密码器
+            //算法/模式/补码方式(PKCS5Padding或NoPadding)
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            //7.初始化密码器，第一个参数为加密(Encrypt_mode)或者解密解密(Decrypt_mode)操作，第二个参数为使用的KEY
+            cipher.init(Cipher.ENCRYPT_MODE, key, ips);
+            //8.获取加密内容的字节数组(这里要设置为utf-8)不然内容中如果有中文和英文混合中文就会解密为乱码
+            byte[] byte_encode = content.getBytes("UTF-8");
+            //9.根据密码器的初始化方式--加密：将数据加密
+            byte[] byte_AES = cipher.doFinal(byte_encode);
+            //10.将加密后的数据转换为字符串
+            return Base64.encodeBase64String(byte_AES);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //如果有错就返加nulll
+        return null;
+    }
+
     public static String AESDecode(String input, byte[] password) {
         try {
             //1.构造密钥生成器，指定为AES算法,不区分大小写
@@ -96,9 +127,7 @@ public class AESEncoder {
             //7.初始化密码器，第一个参数为加密(Encrypt_mode)或者解密(Decrypt_mode)操作，第二个参数为使用的KEY
             cipher.init(Cipher.DECRYPT_MODE, key);
 
-            /*
-             * 解密
-             */
+            // 解密
             byte[] byte_decode = cipher.doFinal(byte_content);
             return new String(byte_decode, "utf-8");
         } catch (NoSuchAlgorithmException e) {
@@ -119,9 +148,48 @@ public class AESEncoder {
         return null;
     }
 
+    public static String AESDecode(String input, byte[] password, byte[] iv) {
+        try {
+            //8.将加密并编码后的内容解码成字节数组
+            byte[] byte_content = Base64.decodeBase64(input);
+
+            //5.根据字节数组生成AES密钥
+            SecretKey key = new SecretKeySpec(password, "AES");
+            //使用CBC模式，需要一个向量iv，可增加加密算法的强度
+            IvParameterSpec ips = new IvParameterSpec(iv);
+            //6.根据指定算法AES自成密码器
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            //7.初始化密码器，第一个参数为加密(Encrypt_mode)或者解密(Decrypt_mode)操作，第二个参数为使用的KEY
+            cipher.init(Cipher.DECRYPT_MODE, key, ips);
+
+            //解密
+            byte[] byte_decode = cipher.doFinal(byte_content);
+            return new String(byte_decode, "utf-8");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //如果有错就返加nulll
+        return null;
+    }
+
     public static void main(String[] args) throws Exception {
         String content = "要加密的内容";
         byte[] password = "9588028820109132".getBytes();
+        //加密向量
+        String iv = "sks0123456789012";
 
 
         //1.构造密钥生成器，指定为AES算法,不区分大小写
@@ -137,9 +205,13 @@ public class AESEncoder {
 
         String encodeResult = AESEncoder.AESEncode(content, password);
         System.out.println("加密后的结果:" + encodeResult);
-
         String dicodeResult = AESEncoder.AESDecode(encodeResult, password);
         System.out.println("解密后的明文是：" + dicodeResult);
+
+        String encodeResult2 = AESEncoder.AESEncode(content, password, iv.getBytes("UTF-8"));
+        System.out.println("加密后的结果:" + encodeResult2);
+        String dicodeResult2 = AESEncoder.AESDecode(encodeResult2, password, iv.getBytes("UTF-8"));
+        System.out.println("解密后的明文是：" + dicodeResult2);
     }
 
 }
